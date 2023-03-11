3062bfd69ac4b46b887bcd5798fee1b64814fc5b
morin <alexeymorin@gmail.com>
2023-03-11 08:27:45 +0300

    Добавил элементы ввода.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
 
+const fetchData = () => {
+  // REST API call
+}
+
 function App() {
+  const [keyword, setKeyword] = useState('');
+  const [data, setData] = useState([]);
   return (
     <div className="App">
-
+      <input value={keyword} onChange={e => setKeyword(e.target.value)}></input>
+      <button onClick={fetchData}>Fetch</button>
     </div>
   );
 }
