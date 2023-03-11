2bb2d9bc4aa125fba3b1c131c5cc49f00ec851b5
morin <alexeymorin@gmail.com>
2023-03-11 09:17:50 +0300

    Добавил вывод результатов поиска в таблицу.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const fetchData = () => {
-  // REST API call
-}
-
 function App() {
   const [keyword, setKeyword] = useState('');
   const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch('https://api.github.com/search/repositories?q=${keyword}')
+    .then(res => res.json())
+    .then(data => setData(data.items))
+    .catch(err => console.error(err));
+  }
+
   return (
     <div className="App">
       <input value={keyword} onChange={e => setKeyword(e.target.value)}></input>
       <button onClick={fetchData}>Fetch</button>
+      <table>
+        <tbody>
+          {
+            data.map(repo => 
+             <tr>
+              <td>{repo.full_name}</td> 
+              <td>
+                <a href={repo.html_url}>{repo.html_url}</a>
+              </td>
+             </tr> 
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
